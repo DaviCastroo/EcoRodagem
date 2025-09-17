2b0e0bb321d1114478cc6af176b1aff4c81a7d0c
Davi Castro <51200754+DaviCastroo@users.noreply.github.com>
2025-09-17 16:36:04 -0300

    gitignore
    .

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.dashboard, name='dashboard'),
     path('veiculos/', views.veiculos, name='veiculos'),
     path('cadastrar-veiculo/', views.cadastrar_veiculo, name='cadastrar_veiculo'),
+
     # Adicione mais URLs conforme necessário
     # path('abastecimentos/', views.abastecimentos, name='abastecimentos'),
     # path('cadastrar-abastecimento/', views.cadastrar_abastecimento, name='cadastrar_abastecimento'),
