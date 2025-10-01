3de212e15e3ce4d5ecbd9afa443ffe14f31175d6
Davi Castro <51200754+DaviCastroo@users.noreply.github.com>
2025-10-01 14:51:33 -0300

    .
    .

--- ecorodagem/urls.py
+++ ecorodagem/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('/dashboard/'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('polls.urls')),
